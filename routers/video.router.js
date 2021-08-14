787378a8137d862caf5d6367f0c54ae00635ef7a
puneetsingh99 <99singhpuneet99@gmail.com>
2021-08-14 14:30:06 +0530

    feat: get video by id

--- routers/video.router.js
+++ routers/video.router.js
@@ -1,12 +1,19 @@
 const express = require("express");
+
 const {
   addVideo,
   getAllVideos,
   removeVideo,
+  videoIdCheck,
+  getVideo,
 } = require("../controllers/video.controller");
 
 const videoRouter = express.Router();
 
 videoRouter.route("/").get(getAllVideos).post(addVideo).delete(removeVideo);
+
+videoRouter.param("videoId", videoIdCheck);
+
+videoRouter.route("/:videoId").get(getVideo);
 
 module.exports = { videoRouter };
